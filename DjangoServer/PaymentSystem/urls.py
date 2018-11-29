77eb16c06cd1c10ddb851fdcbd59f7b05ee1d9ad
je941004 <je941004@naver.com>
2018-11-30 06:02:08 +0900

    #19 url 수정 및 view(상품 조회) api 완료

--- DjangoServer/PaymentSystem/urls.py
+++ DjangoServer/PaymentSystem/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('payments', include('payment.url')),
-    path('products', include('Product.url'))
+    path('payments/', include('payment.url')),
+    path('products/', include('Product.url'))
 ]
--- DjangoServer/Product/views.py
+++ DjangoServer/Product/views.py
@@ -1,5 +1,4 @@
 from rest_framework.response import Response
-from .serializers import PaymentSerializer
 from rest_framework.decorators import api_view
 from payment.models import Product
 from rest_framework import status
@@ -11,5 +10,5 @@ def Product_search(request):
     if request.method == 'GET':
         url= request.data['product_url']
         product = Product.objects.get(product_url=url)
-        print(model_to_dict(product))
-        return Response({"code": "Success", "msg": "상품이 조회되었습니다.","results":[]},status=status.HTTP_200_OK)
+        product_dict = model_to_dict(product)
+        return Response({"code": "Success", "msg": "상품이 조회되었습니다.","results":[product_dict]},status=status.HTTP_200_OK)

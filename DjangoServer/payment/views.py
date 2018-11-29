907ad69347e87fe4778dd7d8f7acd0cb11699da8
je941004 <je941004@naver.com>
2018-11-30 06:27:31 +0900

    #19 url 수정 및 view(상품 조회) api 최종 완료

--- DjangoServer/payment/views.py
+++ DjangoServer/payment/views.py
@@ -22,13 +22,15 @@ def Payment_create(request):
 def Payment_update(request,payment_id):
     if request.method == 'PUT':
         origin_payment_id = request.data['payment_id']
+        # url로 접속한 payment와 요청한 payment가 같은지 확인
         if int(payment_id) == int(origin_payment_id):
             payment_state = request.data['payment_state']
             payment = Payment.objects.get(id=payment_id)
+            #payment상태 업데이트
             payment.payment_state = payment_state
             payment.save()
             payment_obj = model_to_dict(payment)
-            return Response({"code": "Success", "msg": "결제 정보가 업데이트 되었습니다.","results": request.data}, status=status.HTTP_200_OK)
+            return Response({"code": "Success", "msg": "결제 정보가 업데이트 되었습니다.","results": payment_obj}, status=status.HTTP_200_OK)
         else:
             return Response({"code": "Fail", "msg": "결제번호가 달라졌습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
